69c8162863655fa17d7e60a6731bb1922c5d1928
Silas Jimmy <jimmysilas17@gmail.com>
2022-06-15 16:20:47 +0300

    feat: Add vuetify and pinia

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,10 +1,19 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import { createPinia, PiniaVuePlugin } from 'pinia'
+import VueCompositionAPI from '@vue/composition-api'
+import vuetify from './plugins/vuetify'
+
+Vue.use(PiniaVuePlugin)
+const pinia = createPinia()
+Vue.use(VueCompositionAPI)
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
+  pinia,
+  vuetify,
   render: h => h(App)
 }).$mount('#app')
